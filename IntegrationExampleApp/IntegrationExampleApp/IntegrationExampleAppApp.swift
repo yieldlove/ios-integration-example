bb369dca19f9b347236fa5935b78296624f4ee11
m-bednar <mbedna8@seznam.cz>
2022-06-23 09:21:30 +0200

    create empty example app

---
+++ IntegrationExampleApp/IntegrationExampleApp/IntegrationExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationExampleAppApp.swift
+//  IntegrationExampleApp
+//
+//  Created by Martin Bednář on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
