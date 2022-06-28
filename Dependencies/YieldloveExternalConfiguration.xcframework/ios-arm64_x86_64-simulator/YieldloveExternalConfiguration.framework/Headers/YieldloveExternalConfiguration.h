2e9c893dd437c595d765731d79a155469f37a4cd
m-bednar <mbedna8@seznam.cz>
2022-06-28 10:58:52 +0200

    add external-configuration dependency

---
+++ Dependencies/YieldloveExternalConfiguration.xcframework/ios-arm64_x86_64-simulator/YieldloveExternalConfiguration.framework/Headers/YieldloveExternalConfiguration.h
@@ -0,0 +1,18 @@
+//
+//  YieldloveExternalConfiguration.h
+//  YieldloveExternalConfiguration
+//
+//  Created by Marcel Klínek on 01.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YieldloveExternalConfiguration.
+FOUNDATION_EXPORT double YieldloveExternalConfigurationVersionNumber;
+
+//! Project version string for YieldloveExternalConfiguration.
+FOUNDATION_EXPORT const unsigned char YieldloveExternalConfigurationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YieldloveExternalConfiguration/PublicHeader.h>
+
+
