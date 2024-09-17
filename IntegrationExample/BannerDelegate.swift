53a6f93e917922e3d1ec2850b7336bdb7d2899ec
Hyungon Kim <hyungon.kim@adscale.co.nz>
2024-09-17 14:28:55 +1200

    Fixed the memory leak and clean up.

--- IntegrationExample/BannerDelegate.swift
+++ IntegrationExample/BannerDelegate.swift
@@ -4,12 +4,10 @@ import GoogleMobileAds
 
 class BannerDelegate: NSObject, YLBannerViewDelegate {
     
-    private var adSlotId: String?
-
+    private var adSlotId: String
     
-    private let viewController: ViewController?
-    
-    weak var adView: GADBannerView?
+    private weak var viewController: ViewController?
+    private weak var adView : GADBannerView?
     
     init(adSlotId: String, viewController: ViewController) {
         self.adSlotId = adSlotId
@@ -17,33 +15,37 @@ class BannerDelegate: NSObject, YLBannerViewDelegate {
     }
 
     public func bannerViewDidReceiveAd(_ ylBannerView: YLBannerView) {
-        let bannerView = ylBannerView.getBannerView()
+        self.adView = ylBannerView.getBannerView()
         
         // Add banner as a subview
-        viewController?.view.addSubview(bannerView)
-        
-        // This line is needed to resize ads that may come from Prebid
-        Yieldlove.instance.resizeBanner(banner: ylBannerView)
+        if let adView = self.adView
+        {
+            viewController?.view.addSubview(adView)
+            
+            // This line is needed to resize ads that may come from Prebid
+            Yieldlove.instance.resizeBanner(banner: ylBannerView)
+        }
     }
 
     public func bannerView(_ bannerView: YLBannerView, didFailToReceiveAdWithError error: Error) {
         print("ERROR: \(error.localizedDescription)")          // Print error, that happened while loading ad
     }
     
+    
     public func load()  {
-         if let adSlot = adSlotId, let vc = viewController {
-             Yieldlove.instance.bannerAd(
-                adSlotId: adSlot,
-                viewController: vc,
-                delegate: self
-             )
-         }
+         Yieldlove.instance.bannerAd(
+            adSlotId: adSlotId,
+            viewController: viewController!,
+            delegate: self
+         )
      }
     
-    func clearBanner() {
-        adView?.removeFromSuperview()
-        adView?.delegate = nil
+    public func clearBanner()
+    {
+        adView?.removeFromSuperview();
+        adView = nil
     }
+    
     
     func getGAMRequest() -> GAMRequest {
         let publishersRequest = GAMRequest()
--- IntegrationExample/ViewController.swift
+++ IntegrationExample/ViewController.swift
@@ -2,24 +2,32 @@ import UIKit
 import YieldloveAdIntegration
 
 class ViewController: UIViewController {
-   
     var bannerView : BannerDelegate?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Test configuration
-        let appName = "mps_motorradonline"
-        let adSlotId = "b1"
-        
+        let appName = "appDfpTest"
         // Setup Yieldlove (this should be done only once)
         Yieldlove.setup(appName: appName)
-        Yieldlove.instance.developerModeEnabled = true
+        // Yieldlove.instance.developerModeEnabled = true
         Yieldlove.instance.enableDebug(isEnabled: true)
-        
-        bannerView = BannerDelegate(adSlotId: adSlotId, viewController: self)
-        bannerView?.load()
-                
+        loadBanner()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        clearBanner()
     }
 
+    func loadBanner() {
+        let adSlotId = "b1"
+
+        bannerView = BannerDelegate(adSlotId: adSlotId, viewController: self)
+        bannerView?.load()
+    }
+
+    func clearBanner() {
+        self.bannerView?.clearBanner()
+        self.bannerView = nil
+    }
 }
